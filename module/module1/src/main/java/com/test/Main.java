9973a70142ce76e6fbc4e7649171bdcf2811e425
Duy Pham Tran <ptduy@mmsofts.com>
2016-04-20 11:26:04 +0700

    no message

--- module/module1/src/main/java/com/test/Main.java
+++ module/module1/src/main/java/com/test/Main.java
@@ -4,10 +4,10 @@ package com.test;
  * Created by ptduy on 4/20/16.
  */
 public class Main {
-    public String title = "TestTrigger";
+    public String title = "TestTrigger1";
 
     public static void main(String[] args){
-        System.out.println("Duy Handsome");
-        System.out.println("Huy Gay");
+        System.out.println("Duy Handsome1");
+        System.out.println("Huy Gay1");
     }
 }
---
+++ module/module2/src/main/java/com/test/Main.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * Created by ptduy on 4/20/16.
+ */
+public class Main {
+    public String title = "TestTrigger2";
+
+    public static void main(String[] args){
+        System.out.println("Duy Handsome2");
+        System.out.println("Huy Gay2");
+    }
+}
