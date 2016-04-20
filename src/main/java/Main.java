5d7e88b179af0f8e857c2638ba42f0ed2b8810cb
Duy Pham Tran <ptduy@mmsofts.com>
2016-04-20 11:14:33 +0700

    change TestTrigger into gradle base project

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ptduy on 4/20/16.
+ */
+public class Main {
+    public String title = "TestTrigger";
+
+    public static void main(String[] args){
+        System.out.println("Duy Handsome");
+        System.out.println("Huy Gay");
+    }
+}
